73395836a25136acb1313db4bbed695b8df3085e
beebeom87 <xoqmfflt2>
2016-08-21 23:06:30 +0900

    Initial Commit

---
+++ app/src/main/java/com/sb/uzcontrol/MainActivity.java
@@ -0,0 +1,18 @@
+package com.sb.uzcontrol;
+
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TabLayout tabLayout = (TabLayout)findViewById(R.id.t)
+    }
+}
