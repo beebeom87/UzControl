2650db1c3f407b39133f511dca9652e9eb82856c
beebeom87 <xoqmfflt2>
2016-08-22 00:16:48 +0900

    Add UI Fragment

---
+++ app/src/main/java/com/sb/uzcontrol/LineTracerFragment.java
@@ -0,0 +1,14 @@
+package com.sb.uzcontrol;
+
+import android.support.v4.app.Fragment;
+import android.view.View;
+
+/**
+ * Created by SB on 2016-08-21.
+ */
+public class LineTracerFragment extends Fragment implements View.OnClickListener{
+    @Override
+    public void onClick(View view) {
+
+    }
+}
--- app/src/main/java/com/sb/uzcontrol/MainActivity.java
+++ app/src/main/java/com/sb/uzcontrol/MainActivity.java
@@ -1,18 +1,92 @@
 package com.sb.uzcontrol;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements TabLayout.OnTabSelectedListener{
 
     ViewPager viewPager;
+
+    // UI stuff
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle toggle;
+
+    private ImageView mImageBT   = null;
+    private TextView  mTextStatus = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TabLayout tabLayout = (TabLayout)findViewById(R.id.t)
+        TabLayout tabLayout = (TabLayout)findViewById(R.id.tab_layout);
+        viewPager = (ViewPager)findViewById(R.id.pager);
+
+        viewPager.setAdapter(new MyPagerAdapter(getSupportFragmentManager()));
+
+        tabLayout.setupWithViewPager(viewPager);
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer);
+
+        // Setup Views
+        mImageBT = (ImageView)findViewById(R.id.status_title);
+        mImageBT.setImageDrawable(getResources().getDrawable(android.R.drawable.presence_invisible));
+        mTextStatus = (TextView)findViewById(R.id.status_text);
+        mTextStatus.setText(getResources().getString(R.string.bt_state_init));
+    }
+
+    @Override
+    public void onTabSelected(TabLayout.Tab tab) {
+        viewPager.setCurrentItem(tab.getPosition());
+    }
+
+    @Override
+    public void onTabUnselected(TabLayout.Tab tab) {
+
+    }
+
+    @Override
+    public void onTabReselected(TabLayout.Tab tab) {
+
+    }
+
+    class MyPagerAdapter extends FragmentPagerAdapter {
+        List<Fragment> fragments = new ArrayList<>();
+
+        String titles[] = new String[]{"Move Control", "Line Tracer", "Setting"};
+
+        public MyPagerAdapter(FragmentManager fm) {
+            super(fm);
+            fragments.add(new MoveControlFragment());
+            fragments.add(new LineTracerFragment());
+            fragments.add(new SettingFragment());
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return fragments.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return fragments.size();
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return titles[position];
+        }
     }
 }
