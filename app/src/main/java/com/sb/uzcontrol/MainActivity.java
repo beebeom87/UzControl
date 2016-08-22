37dfde7c624dd192937c6784612572d9ef960cce
beebeom87 <beebeom87@gmail.com>
2016-08-23 00:52:40 +0900

    Too Sleepy

--- app/src/main/java/com/sb/uzcontrol/MainActivity.java
+++ app/src/main/java/com/sb/uzcontrol/MainActivity.java
@@ -1,22 +1,20 @@
 package com.sb.uzcontrol;
 
+import android.app.ActionBar;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
 import android.os.Handler;
 import android.os.Message;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -36,11 +34,20 @@ public class MainActivity extends AppCompatActivity implements TabLayout.OnTabSe
     private static final int REQUEST_ENABLE_BT = 3;
 
     // UI stuff
-    DrawerLayout drawerLayout;
-    ActionBarDrawerToggle toggle;
 
     private ImageView mImageBT   = null;
+
     private TextView  mTextStatus = null;
+
+    /**
+     * Name of the connected device
+     */
+    private String mConnectedDeviceName = null;
+
+    /**
+     * String buffer for outgoing messages
+     */
+    private StringBuffer mOutStringBuffer;
 
     /**
      * Local Bluetooth adapter
@@ -56,6 +63,8 @@ public class MainActivity extends AppCompatActivity implements TabLayout.OnTabSe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        UZLog.d(LOG_TAG, "onCreate()");
 
         TabLayout tabLayout = (TabLayout)findViewById(R.id.tab_layout);
         viewPager = (ViewPager)findViewById(R.id.pager);
@@ -133,7 +142,7 @@ public class MainActivity extends AppCompatActivity implements TabLayout.OnTabSe
             }
             case R.id.insecure_connect_scan: {
                 // Launch the DeviceListActivity to see devices and do scan
-                Intent serverIntent = new Intent(getActivity(), DeviceListActivity.class);
+                Intent serverIntent = new Intent(this, DeviceListActivity.class);
                 startActivityForResult(serverIntent, REQUEST_CONNECT_DEVICE_INSECURE);
                 return true;
             }
@@ -143,17 +152,17 @@ public class MainActivity extends AppCompatActivity implements TabLayout.OnTabSe
                 return true;
             }
         }
-
-
-            return super.onPrepareOptionsMenu(menu);
+        return false;
     }
 
     public void doBTService() {
+        UZLog.d(LOG_TAG, "doBTService()");
         // Get local Bluetooth adapter
         mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 
         // if the adapter is null, then Bluetooth is not supported
         if(mBluetoothAdapter == null) {
+            UZLog.d(LOG_TAG, "Bluetooth is not available");
             Toast.makeText(this, "Bluetooth is not available", Toast.LENGTH_LONG).show();
             this.finish();
         }
@@ -187,20 +196,20 @@ public class MainActivity extends AppCompatActivity implements TabLayout.OnTabSe
     private final Handler mHandler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
-            FragmentActivity activity = getActivity();
             switch (msg.what) {
                 case Constants.MESSAGE_STATE_CHANGE:
                     switch (msg.arg1) {
                         case BluetoothChatService.STATE_CONNECTED:
+                            mImageBT.setImageDrawable(getResources().getDrawable(android.R.drawable.presence_online));
                             setStatus(getString(R.string.title_connected_to, mConnectedDeviceName));
-                            // mConversationArrayAdapter.clear();
-                            mTextViewMenu.setText("");
                             break;
                         case BluetoothChatService.STATE_CONNECTING:
+                            mImageBT.setImageDrawable(getResources().getDrawable(android.R.drawable.presence_away));
                             setStatus(R.string.title_connecting);
                             break;
                         case BluetoothChatService.STATE_LISTEN:
                         case BluetoothChatService.STATE_NONE:
+                            mImageBT.setImageDrawable(getResources().getDrawable(android.R.drawable.presence_invisible));
                             setStatus(R.string.title_not_connected);
                             break;
                     }
@@ -216,23 +225,117 @@ public class MainActivity extends AppCompatActivity implements TabLayout.OnTabSe
                     // construct a string from the valid bytes in the buffer
                     String readMessage = new String(readBuf, 0, msg.arg1);
                     //mConversationArrayAdapter.add(mConnectedDeviceName + ":  " + readMessage);
-                    mTextViewMenu.append(readMessage);
+                    mTextStatus.append(readMessage);
                     break;
                 case Constants.MESSAGE_DEVICE_NAME:
                     // save the connected device's name
                     mConnectedDeviceName = msg.getData().getString(Constants.DEVICE_NAME);
-                    if (null != activity) {
-                        Toast.makeText(activity, "Connected to "
+                    Toast.makeText(getApplicationContext(), "Connected to "
                                 + mConnectedDeviceName, Toast.LENGTH_SHORT).show();
-                    }
                     break;
                 case Constants.MESSAGE_TOAST:
-                    if (null != activity) {
-                        Toast.makeText(activity, msg.getData().getString(Constants.TOAST),
+                    Toast.makeText(getApplicationContext(), msg.getData().getString(Constants.TOAST),
                                 Toast.LENGTH_SHORT).show();
-                    }
                     break;
             }
         }
     };
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case REQUEST_CONNECT_DEVICE_SECURE:
+                // When DeviceListActivity returns with a device to connect
+                if (resultCode == RESULT_OK) {
+                    connectDevice(data, true);
+                }
+                break;
+            case REQUEST_CONNECT_DEVICE_INSECURE:
+                // When DeviceListActivity returns with a device to connect
+                if (resultCode == RESULT_OK) {
+                    connectDevice(data, false);
+                }
+                break;
+            case REQUEST_ENABLE_BT:
+                // When the request to enable Bluetooth returns
+                if (resultCode == RESULT_OK) {
+                    // Bluetooth is now enabled, so set up a chat session
+                    setupChat();
+                } else {
+                    // User did not enable Bluetooth or an error occurred
+                    UZLog.d(LOG_TAG, "BT not enabled");
+                    Toast.makeText(this, R.string.bt_not_enabled_leaving,
+                            Toast.LENGTH_SHORT).show();
+                    this.finish();
+                }
+        }
+    }
+
+    /**
+     * Establish connection with other divice
+     *
+     * @param data   An {@link Intent} with {@link DeviceListActivity#EXTRA_DEVICE_ADDRESS} extra.
+     * @param secure Socket Security type - Secure (true) , Insecure (false)
+     */
+    private void connectDevice(Intent data, boolean secure) {
+        // Get the device MAC address
+        String address = data.getExtras()
+                .getString(DeviceListActivity.EXTRA_DEVICE_ADDRESS);
+        // Get the BluetoothDevice object
+        BluetoothDevice device = mBluetoothAdapter.getRemoteDevice(address);
+        // Attempt to connect to the device
+        mChatService.connect(device, secure);
+    }
+
+    /**
+     * Updates the status on the action bar.
+     *
+     * @param resId a string resource ID
+     */
+    private void setStatus(int resId) {
+        mTextStatus.setText(resId);
+    }
+
+    /**
+     * Updates the status on the action bar.
+     *
+     * @param subTitle status
+     */
+    private void setStatus(CharSequence subTitle) {
+        mTextStatus.setText(subTitle);
+    }
+
+    /**
+     * Makes this device discoverable.
+     */
+    private void ensureDiscoverable() {
+        if (mBluetoothAdapter.getScanMode() !=
+                BluetoothAdapter.SCAN_MODE_CONNECTABLE_DISCOVERABLE) {
+            Intent discoverableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+            discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300);
+            startActivity(discoverableIntent);
+        }
+    }
+
+    /**
+     * Sends a message.
+     *
+     * @param message A string of text to send.
+     */
+    private void sendMessage(String message) {
+        // Check that we're actually connected before trying anything
+        if (mChatService.getState() != BluetoothChatService.STATE_CONNECTED) {
+            Toast.makeText(this, R.string.not_connected, Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // Check that there's actually something to send
+        if (message.length() > 0) {
+            // Get the message bytes and tell the BluetoothChatService to write
+            byte[] send = message.getBytes();
+            mChatService.write(send);
+
+            // Reset out string buffer to zero and clear the edit text field
+            mOutStringBuffer.setLength(0);
+        }
+    }
 }
--- app/src/main/java/com/sb/uzcontrol/MoveControlFragment.java
+++ app/src/main/java/com/sb/uzcontrol/MoveControlFragment.java
@@ -1,18 +1,22 @@
 package com.sb.uzcontrol;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.LinearLayout;
 
 /**
  * Created by SB on 2016-08-21.
  */
-public class MoveControlFragment extends Fragment implements View.OnClickListener{
+public class MoveControlFragment extends Fragment{
 
     LinearLayout mLinearLayout;
+
+    Button mTestBtn;
 
     public MoveControlFragment() {
         // Required empty public constructor
@@ -29,7 +33,14 @@ public class MoveControlFragment extends Fragment implements View.OnClickListene
     }
 
     @Override
-    public void onClick(View view) {
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        mTestBtn = (Button)view.findViewById(R.id.test_button);
 
+        mTestBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
